46857e11f093e4a746cfeb1f8e15006c1a5cfcbb
linyang <119745743@qq.com>
2018-01-30 15:26:55 +0800

    log 修改

--- examples/log.js
+++ examples/log.js
@@ -1,7 +1,36 @@
-var logger = require('../lib/logger').getLogger('log', __filename, process.pid);
 
-process.env.LOGGER_LINE = true;
-// process.env.RAW_MESSAGE = true;
+let logger1 = require('../lib/logger')
+
+let config = {
+    "appenders": {
+      "console": {
+        "type": "console"
+      },
+      "file": {
+        "type": "file",
+        "filename": "${opts:base}/logs/con-log-${opts:serverId}.log",
+        "maxLogSize": 1048576,
+        "layout": {
+          "type": "basic"
+        },
+        "backups": 5
+      }
+    },
+    "categories": {
+      "default": {
+        "appenders": ["file", "console"],
+        "level": "debug"
+      }
+    },
+    "replaceConsole": true,
+    "lineDebug": true,
+    "rawMessage":false
+  }
+
+  logger1.configure(config);
+
+  var logger = require('../lib/logger').getLogger('default');
+
 logger.info('test1');
 logger.warn('test2');
 logger.error('test3');
